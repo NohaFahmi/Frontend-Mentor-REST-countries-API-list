65bcd0bd049edf4d83e7f9321cf4f37392753ac0
Noha M <33437197+NohaaAa@users.noreply.github.com>
2021-10-22 12:12:45 +0200

    improved countries list logic code

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,18 +1,31 @@
 import { HomeSearch } from "./countries-search/HomeSearch";
 import CountriesList from "./countries-list/CountriesList";
 import React from "react";
-
+import { connect } from "react-redux";
+import * as actions from "../actions";
 
 class Home extends React.Component {
   
   render() {
-    return (
-      <div className="page-content container">
-        <HomeSearch></HomeSearch>
-        <CountriesList></CountriesList>
-      </div>
-    );
+    if(this.props.list) {
+      const countriesList = this.props.list['countriesList'];
+      return (
+        <div className="page-content container">
+          <HomeSearch></HomeSearch>
+          <CountriesList countries={countriesList}></CountriesList>
+        </div>
+      );
+    }
   }
+  componentDidMount = () => {
+    this.props.getAllCountries();
+  };
 }
 
-export default Home;
+const mapStateToProps = (state) => {
+  return {
+      list: state.countries
+  };
+};
+
+export default connect(mapStateToProps, actions)(Home);
--- src/components/countries-list/CountriesList.js
+++ src/components/countries-list/CountriesList.js
@@ -1,17 +1,15 @@
 import React from "react";
 import { CountryCard } from "../country-card/CountryCard";
 import './CountriesList.scss';
-import { connect } from "react-redux";
-import * as actions from "../../actions";
 import {Link} from 'react-router-dom';
 
 class CountriesList extends React.Component {
 
-    renderAllCountries({list}) {
-        if(list['countriesList']) {
-            return list['countriesList'].map((country) => {
+    renderAllCountries({countries}) {
+        if(countries) {
+            return countries.map((country) => {
                 return (
-                    <Link to={`/${country.name}`}>
+                    <Link to={`/${country.name}`} key={country.name}>
                         <div className='country-card-item'>
                             <CountryCard country={country}></CountryCard>
                         </div>
@@ -27,14 +25,6 @@ class CountriesList extends React.Component {
                 </div>
             )
     }
-    componentDidMount = () => {
-        this.props.getAllCountries();
-      };
     
 }
-const mapStateToProps = (state) => {
-    return {
-        list: state.countries
-    };
-  };
-export default connect(mapStateToProps, actions)(CountriesList);
+export default CountriesList;
